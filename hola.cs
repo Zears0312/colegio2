4d37779730d7c9ee9ec276be2724240f0d975e5e
Alumno <alumno@et26.local>
2026-04-01 21:08:57 -0300

    añadi cs

---
+++ hola.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hola mundo");
+        Console.WriteLine("Tocar una tecla para salir");
+        Console.ReadKey();
+    }
+}
